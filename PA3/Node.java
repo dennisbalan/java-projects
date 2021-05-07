bcab5e67d56083d78c7f6207ee71de7575e2a4d0
dennisbalan <dbalan2234@gmail.com>
2021-05-07 02:43:26 -0700

    Git commit 2

--- PA3/Node.java
+++ PA3/Node.java
@@ -1,12 +1,18 @@
+//	Dennis Balan CSE223 PA3 5/7/2021   Node is just class that more like a C struct. It exists soleley as a way to store information. It is a node in the Tree object and stores the text, the flag on whether it is a question or answer (question true, answer false) and left and right subtrees and yes and no rspecyively. Constructors for no inputs and variants with text and flag are the only things inside
 class Node{
+	//string text stores the question answer
 	String text;
+	//flag indicates what kind a question or answer is, true is question, false is answer
 	boolean flag;	
+	//yes is left sub tree
 	Node yes;
+	//no is right sub tree
 	Node no;
-
+	//constructor for no inputs sets left and right childs (yes and no) to null
 	public Node(){
 		yes=no=null;
 	}
+	//constructor fpr a new Node with info. // takes inputs strings new_text and boolean new_flag and assigns them to text and flag respectivelyt
 	public Node(String new_text,boolean new_flag){
 		text = new_text;
 		flag = new_flag;
